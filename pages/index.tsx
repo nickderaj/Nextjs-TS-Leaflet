2c674cbb4142ef2c08cfa1e42adbe073ccd6e3eb
nickderaj <nickderaj@gmail.com>
2022-08-17 16:06:36 +0800

    feat: updated home page to display map and controls

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,49 @@
 import Button from '@/components/elements/buttons/Button';
 import PrimaryLayout from '@/components/layouts/PrimaryLayout';
-import SampleModal from '@/components/modals/SampleModal';
-import { setSampleModalOpen } from '@/redux/slices/modalSlice';
+import Map from '@/components/map';
+import { setDrawMode } from '@/redux/slices/drawSlice';
+import { RootState } from '@/redux/store';
 import { PageWithLayout } from '@/types/page';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 export default function Home(_: PageWithLayout) {
+  const { drawMode } = useSelector((state: RootState) => state.draw);
   const dispatch = useDispatch();
 
   return (
     <>
-      <section className="flex justify-center items-center h-screen w-screen">
-        <Button onClick={() => dispatch(setSampleModalOpen(true))} title="OPEN MODAL" />
+      <section className="h-screen w-screen flex">
+        <div className="h-full w-full">
+          <Map />
+        </div>
+        <div className="w-3/12 flex flex-col pt-16 gap-2 px-24 border-l border-l-indigo-500">
+          <Button
+            title="Rectangle"
+            onClick={() => dispatch(setDrawMode('rectangle'))}
+            variant={drawMode === 'rectangle' ? 'primary' : 'secondary'}
+          />
+          <Button
+            title="Polygon"
+            onClick={() => dispatch(setDrawMode('polygon'))}
+            variant={drawMode === 'polygon' ? 'primary' : 'secondary'}
+          />
+          <Button
+            title="Polyline"
+            onClick={() => dispatch(setDrawMode('polyline'))}
+            variant={drawMode === 'polyline' ? 'primary' : 'secondary'}
+          />
+          <Button
+            title="Marker"
+            onClick={() => dispatch(setDrawMode('marker'))}
+            variant={drawMode === 'marker' ? 'primary' : 'secondary'}
+          />
+          <Button
+            title="Circle"
+            onClick={() => dispatch(setDrawMode('circle'))}
+            variant={drawMode === 'circle' ? 'primary' : 'secondary'}
+          />
+        </div>
       </section>
-      <SampleModal />
     </>
   );
 }
